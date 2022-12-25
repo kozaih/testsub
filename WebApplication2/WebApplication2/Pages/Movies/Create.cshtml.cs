9313ece93b5171270247ed05b8682af6362e63c7
matcha sisho <matcha_sisho@yahoo.co.jp>
2022-12-25 16:06:44 +0900

    sub application add

---
+++ WebApplication2/WebApplication2/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApplication2.Data.WebApplication2Context _context;
+
+        public CreateModel(WebApplication2.Data.WebApplication2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
