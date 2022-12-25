a817b0505da56baa216fc18e65cab563a7b69c34
matcha sisho <matcha_sisho@yahoo.co.jp>
2022-12-25 16:24:13 +0900

    testusub commit コメント

--- WebApplication2/WebApplication2/Program.cs
+++ WebApplication2/WebApplication2/Program.cs
@@ -20,5 +20,7 @@ namespace WebApplication2
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>();
+
+		// コメントコメントコメント
 	}
 }
